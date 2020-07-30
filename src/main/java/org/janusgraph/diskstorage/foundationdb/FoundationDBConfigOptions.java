fa0d1b9ae5efcc491d93504e87313c175b09b84b
Florian Grieskamp <florian.grieskamp@gdata.de>
2020-07-30 08:02:10 +0200

    Bump FDB API level
    Fix #43
    
    Signed-off-by: Florian Grieskamp <florian.grieskamp@gdata.de>

--- src/main/java/org/janusgraph/diskstorage/foundationdb/FoundationDBConfigOptions.java
+++ src/main/java/org/janusgraph/diskstorage/foundationdb/FoundationDBConfigOptions.java
@@ -45,7 +45,7 @@ public interface FoundationDBConfigOptions {
         "version",
         "The version of the FoundationDB cluster.",
         ConfigOption.Type.LOCAL,
-        520);
+        620);
 
     ConfigOption<String> CLUSTER_FILE_PATH = new ConfigOption<>(
         FDB_NS,
